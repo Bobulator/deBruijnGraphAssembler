010c63a5b49ccf94bcf0c3033eea5a37d1678bb5
Chad <chadsbacon@gmail.com>
2016-10-21 17:23:08 -0600

    Implemented a more robust way of determining kmer coverage. Added some more steps to the TODO file.

--- contigGenerator.py
+++ contigGenerator.py
@@ -3,7 +3,7 @@ import sys
 
 def write_results(result_file, results):
     if type(results) is list:
-        results = "->".join(results)
+        results = "\n".join(results)
 
     with open(result_file, 'w') as f:
         f.write(results)
@@ -19,20 +19,19 @@ def read_fasta_file(input_file):
 
 
 def kmer_counts(reads, k, threshold=0):
-    kmer_counts = {}
+    kmer_occurence_read_indices = {}
 
-    for read in reads:
+    for index, read in enumerate(reads):
         for kmer in [read[i:i + k] for i in xrange(len(read) - k + 1)]:
-            if kmer not in kmer_counts:
-                kmer_counts[kmer] = 0
+            if kmer not in kmer_occurence_read_indices:
+                kmer_occurence_read_indices[kmer] = set()
 
-            kmer_counts[kmer] += 1
+            kmer_occurence_read_indices[kmer].add(index)
 
     results = []
-    for kmer, count in kmer_counts.iteritems():
-        if count >= threshold:
-            for i in xrange(count):
-                results.append(kmer)
+    for kmer, read_indices in kmer_occurence_read_indices.iteritems():
+        if len(read_indices) >= threshold:
+            results.append(kmer)
 
     return results
 
@@ -67,93 +66,63 @@ def find_branching_nodes(graph):
                 edge_counts[edge] = [0, 0]
             edge_counts[edge][0] += 1
 
-    return [node for node, edges in edge_counts.items() if not (edges[0] == 1 and edges[1] == 1)]
+    return [node for node, degrees in edge_counts.items() if not (degrees[0] == 1 and degrees[1] == 1)]
 
 
-def eulerian_cycle(edge_dict):
-    current_node = edge_dict.keys()[0]
-    path = [current_node]
-
-    # Get the initial cycle.
-    while True:
-        path.append(edge_dict[current_node][0])
-
-        if len(edge_dict[current_node]) == 1:
-            del edge_dict[current_node]
-        else:
-            edge_dict[current_node] = edge_dict[current_node][1:]
-
-        if path[-1] in edge_dict:
-            current_node = path[-1]
-        else:
-            break
-
-    # Continually expand the initial cycle until we're out of edge_dict.
-    while len(edge_dict) > 0:
-        for i in xrange(len(path)):
-            if path[i] in edge_dict:
-                current_node = path[i]
-                cycle = [current_node]
-                while True:
-                    cycle.append(edge_dict[current_node][0])
-
-                    if len(edge_dict[current_node]) == 1:
-                        del edge_dict[current_node]
-                    else:
-                        edge_dict[current_node] = edge_dict[current_node][1:]
-
-                    if cycle[-1] in edge_dict:
-                        current_node = cycle[-1]
-                    else:
-                        break
-
-                path = path[:i] + cycle + path[i+1:]
-                break
-    return path
-
-
-def eulerian_path(edge_dict):
-    # Determine the unbalanced edges.
-    out_values = reduce(lambda a, b: a + b, edge_dict.values())
-    for node in set(out_values+edge_dict.keys()):
-        out_value = out_values.count(node)
-        if node in edge_dict:
-            in_value = len(edge_dict[node])
-        else:
-            in_value = 0
-
-        if in_value < out_value:
-            unbalanced_from = node
-        elif out_value < in_value:
-            unbalanced_to = node
-
-    # Add an edge connecting the unbalanced edges.
-    if unbalanced_from in edge_dict:
-        edge_dict[unbalanced_from].append(unbalanced_to)
-    else:
-        edge_dict[unbalanced_from] = [unbalanced_to]
-
-    # Get the Eulerian Cycle from the edges, including the unbalanced edge.
-    cycle = eulerian_cycle(edge_dict)
-
-    # Find the location of the unbalanced edge in the eulerian cycle.
-    divide_point = filter(lambda i: cycle[i:i+2] == [unbalanced_from, unbalanced_to], xrange(len(cycle)-1))[0]
-
-    # Remove the unbalanced edge, and shift appropriately, overlapping the head and tail.
-    return cycle[divide_point+1:] + cycle[1:divide_point+1]
-
-
-def generate_eulerian_path(input_file):
-
+def generate_contigs(input_file, k):
     reads = read_fasta_file(input_file)
-    kmers = kmer_counts(reads=reads, k=6)
+    kmers = kmer_counts(reads, k)
     graph = build_de_bruijn_graph(kmers)
-    #branching_nodes = find_branching_nodes(graph)
+    branching_nodes = find_branching_nodes(graph)
 
-    result = eulerian_path(graph)
-    return result[0] + ''.join([result[i][-1] for i in xrange(1, len(result))])
+    contigs = []
+    for node in branching_nodes:
+        if node not in graph:
+            continue
+
+        for exit_edge in graph[node]:
+            contig = list(node)
+
+            current_node = exit_edge
+            while current_node not in branching_nodes:
+                contig.append(current_node[-1])
+                current_node = graph[current_node][0]
+
+            contig.append(current_node[-1])
+            contigs.append(''.join(contig))
+
+    contigs.sort()
+    return list(set(contigs))
+    #return assembleContigs([list(x) for x in contigs], k)
+
+
+def assembleContigs(contigs, k):
+
+    for kmer_size in xrange(k, 0, -1):
+
+        len_changed = True
+        while len_changed:
+            temp = len(contigs)
+
+            for i in xrange(len(contigs)):
+                if not contigs[i]:
+                    continue
+
+                for j in [x for x in xrange(len(contigs)) if x != i]:
+                    if contigs[i][-kmer_size + 1:] == contigs[j][:kmer_size - 1]:
+                        contigs[i] += contigs[j][kmer_size - 1:]
+                        contigs[j] = []
+
+            contigs = [x for x in contigs if x]
+            print len(contigs)
+            if temp == len(contigs):
+                len_changed = False
+
+    return [''.join(x) for x in contigs]
 
 
 if __name__ == "__main__":
-    c = generate_eulerian_path(sys.argv[1])
+    k = 68
+    #c = generate_eulerian_path(sys.argv[1])
+    c = generate_contigs(sys.argv[1], k)
     write_results("tests\\result-output.txt", c)
